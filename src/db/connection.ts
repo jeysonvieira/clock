fefc56ac8cde36c52fa89575a7fb89b1e9fcf522
Jeyson <jeysonsaraiva@gmail.com>
2023-08-17 12:08:49 -0300

    mongoDB bug fixed

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/clock')
+    await mongoose.connect('mongodb://127.0.0.1:27017/clock')
     console.log('CONNECT WITH MONGOOSE')
 }
 
